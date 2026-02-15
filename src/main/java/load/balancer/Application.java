3738c6c27265c3dbacd138afb660294cd041caac
Aleksey-S-58 <ascanada52@gmail.com>
2026-02-15 03:28:49 +0300

    assembly plugin

---
+++ src/main/java/load/balancer/Application.java
@@ -0,0 +1,13 @@
+package load.balancer;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        printWelcomeMessage();
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("Application started");
+    }
+}
