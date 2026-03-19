a42cbf45bf919dc724b697244db36735540ea0c4
Aleksey-S-58 <ascanada52@gmail.com>
2026-03-20 02:21:46 +0300

    abstract transmitter

--- src/main/java/load/balancer/transmitter/AbstractTransmitter.java
+++ src/main/java/load/balancer/transmitter/AbstractTransmitter.java
@@ -4,13 +4,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.slf4j.Logger;
+
+import load.balancer.socket.connector.SocketConnector;
+
 public abstract class AbstractTransmitter implements Runnable {
+
+    protected final SocketConnector inputConnector;
+    protected final SocketConnector outputConnector;
+    protected final String name;
+
+    protected AbstractTransmitter(SocketConnector inputConnector, SocketConnector outputConnector, String name) {
+        this.name = name;
+        this.inputConnector = inputConnector;
+        this.outputConnector = outputConnector;
+    }
 
     @Override
     public void run() {
-        // TODO call method transmit
+        try {
+            InputStream inputStream = inputConnector.getInputStream();
+            OutputStream outputStream = outputConnector.getOutputStream();
+            transmit(inputStream, outputStream);
+            inputConnector.close();
+            outputConnector.close();
+        } catch (IOException e) {
+            getLogger().error("Transmitter {} can't read/write from connection: {}", name, e);
+        }
     }
 
     protected abstract void transmit(InputStream inputStream, OutputStream outputStream) throws IOException;
+    protected abstract Logger getLogger();
 
 }
